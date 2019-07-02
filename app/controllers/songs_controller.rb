ede92897c0df909ed17d5534b416cb819da9f298
surpreet.mundra <surpreet.mundra@gmail.com>
2019-07-02 14:32:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,7 +29,7 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug/edit' do
-    @song = Song.find_by_slug(:slug)
+    @song = Song.find_by_slug(params[:slug])
     erb :'songs/edit'
   end
 
